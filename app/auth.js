6b89d58343e569b518a12bb5e15500e6a9ee3293
Nicolas Pantoja <nicolaspantoja@hotmail.com>
2022-11-16 07:42:54 -0400

    update

--- app/auth.js
+++ app/auth.js
@@ -1,5 +1,5 @@
 if(!sessionStorage.getItem("id") ){
-  document.location.href = "../../Tela_Login/login.html";
+  document.location.href = "./../ Tela_Login/login.html";
 } else {
   document.location.href = "../Tela_Seleção de perfil/tp.html";
 }
